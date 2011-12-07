c3bd833518a05e1b5a8173c2d6d466e7609451a0
Steven McCoy <steven.mccoy@thomsonreuters.com>
2011-12-07 14:26:49 +0000

    Decode FlexRecord blob and dump both string and enumerated content.

--- src/velocity_receiver.cc
+++ src/velocity_receiver.cc
@@ -84,21 +84,115 @@ temp::velocity_receiver_t::processFlexRecord (
 	std::unique_ptr<vpf::FlexRecordEvent> event_
 	)
 {
-	const uint32_t fr_id = event_->getFlexRecBase()->fDefinitionId;
+	const uint32_t record_id = event_->getFlexRecBase()->fDefinitionId;
 
 /* verify we have the FlexRecord identifier to decompress the contents */
-	const auto it = flexrecord_map.find (fr_id);
+	const auto it = flexrecord_map.find (record_id);
 	if (it == flexrecord_map.end()) {
 		temp_warn ("Unknown FlexRecord id %" PRIu32 " on symbol name '%s'",
-			fr_id,
+			record_id,
 			event_->getSymbolName());
 		unknown_flexrecord_count++;
 		return;
 	}
 
-	temp_info ("symbol name=%s, flexrecord id=%" PRIu32,
+{
+const FlexRecBaseTickData* base = event_->getFlexRecBase();
+vpf::FlexRecData* data = it->second.get();
+char buffer[1024];
+
+VarFieldsView* tick = data->deblobToView (event_->getFlexRecBlob());
+if (nullptr == tick) {
+	temp_warn ("tick is nullptr");
+}
+
+data->toString (buffer);
+	temp_info ("symbol name=%s, flexrecord id=%" PRIu32 " dump=%s",
 		event_->getSymbolName(),
-		fr_id);
+		record_id
+,buffer);
+}
+
+	const FlexRecBaseTickData* base = event_->getFlexRecBase();
+	const vpf::FlexRecData* data = it->second.get();
+	const VarFieldsView* fields = data->getFieldsView();
+	const int fieldCount = data->getFieldCount();
+
+	for (int i = kFRFixedFields;
+		i < fieldCount;
+		++i)
+	{
+		if (!fields[i].length) {
+			temp_info ("field: %d", i);
+			continue;
+		}
+		switch (fields[i].theType) {
+		case VarFieldsEnums::t_NOTYPE:
+		case VarFieldsEnums::t_NOTYPE2:
+			temp_info ("field: %d, type:\"(nul)\"", i);
+			break;
+		case VarFieldsEnums::t_string:
+			temp_info ("field: %d, type:\"string\", value=\"%s\"", i, (const char*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_float:
+			temp_info ("field: %d, type:\"float\", value=%f", i, *(const float*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_double:
+		case VarFieldsEnums::t_doubleReally:
+			temp_info ("field: %d, type:\"double\", value=%g", i, *(const double*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_s8:
+			temp_info ("field: %d, type:\"int8_t\", value=%d", i, *(const int8_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_u8:
+			temp_info ("field: %d, type:\"uint8_t\", value=%u", i, *(const uint8_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_s16:
+			temp_info ("field: %d, type:\"int16_t\", value=%d", i, *(const int16_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_u16:
+			temp_info ("field: %d, type:\"uint16_t\", value=%u", i, *(const uint16_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_s32:
+			temp_info ("field: %d, type:\"int32_t\", value=%d", i, *(const int32_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_u32:
+			temp_info ("field: %d, type:\"uint32_t\", value=%u", i, *(const uint32_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_s64:
+			temp_info ("field: %d, type:\"int64_t\", value=%d", i, *(const int64_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_u64:
+			temp_info ("field: %d, type:\"uint64_t\", value=%u", i, *(const uint64_t*)fields[i].data);
+			break;
+		case VarFieldsEnums::t_byte:
+			temp_info ("field: %d, type:\"byte array\"", i);
+			break;
+		case VarFieldsEnums::t_s:
+			temp_info ("field: %d, type:\"varint\"", i);
+			break;
+		case VarFieldsEnums::t_u:
+			temp_info ("field: %d, type:\"varuint\"", i);
+			break;
+		case VarFieldsEnums::t_PD:
+			temp_info ("field: %d, type:\"packed decimal\"", i);
+			break;
+		case VarFieldsEnums::t_time_t:
+			temp_info ("field: %d, type:\"time_t\"", i);
+			break;
+		case VarFieldsEnums::t_VHTime:
+		case VarFieldsEnums::t_NTTime:
+			temp_info ("field: %d, type:\"FILETIME\"", i);
+			break;
+		case VarFieldsEnums::t_UNICODE:
+			temp_info ("field: %d, type:\"unicode string\"", i);
+			break;
+		default:
+			temp_info ("field: %d, type:\"(unknown)\"", i);
+			break;
+		}
+			
+	}
 }
 
 /* Enumerate the FlexRecord dictionary from the Analytic Engine process.
